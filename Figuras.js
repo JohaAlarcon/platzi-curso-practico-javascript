5deba5625e7e4a969cac2e56a2272db9301d70b6
JohanaAlarcon <sanljfray555@gmail.com>
2021-08-05 18:05:09 -0500

    Ejercicio de figuras

---
+++ Figuras.js
@@ -0,0 +1,21 @@
+//Codigo para el cuadrado
+
+const ladoCuadrado = 5;
+console.log ("Los lados del cuadrado miden :"+ladoCuadrado + "cm")
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log ("El perimetro del cuadrado es :"+perimetroCuadrado+ "cm")
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log ("El area del cuadrado es :"+areaCuadrado+ "cm")
+
+//Codigo para el triangulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 5;
+const baseTriangulo = 4;
+console.log (
+    "Los lados del triangulo miden :"
+    +ladoTriangulo1 + "cm, "
+    +ladoTriangulo2 + "cm, "
+     +baseTriangulo + "cm, " )
